684249cbf4d69f2c2359eb767585d001fe08c8b3
Subodh Kumar ES <subodh.kumar@wavemaker.com>
2020-03-02 14:36:56 +0530

    Removed angular.json template

--- wm_replace_angular.js
+++ wm_replace_angular.js
@@ -6,11 +6,20 @@ const { getSspaPath } = require("./wm_utils");
 const getAngularJsonPath = path => {
   return node_path.resolve(path ? `${getSspaPath(path)}/angular.json` : "");
 };
-const wmTemplate = require(`./wm_template`);
-const replaceAngularJson = proj_path => {
-  const dest = getAngularJsonPath(proj_path);
-  fs.writeFileSync(dest, wmTemplate.ngJsonTemplate(), "utf-8");
-};
+const removeLazyEntries = options => options.map((op)=>(typeof op==="object"?op["input"]:op));
+const replaceAngularJson = proj_path =>{
+  const src_path = getAngularJsonPath(proj_path);
+  const ng_json = require(src_path);
+  const build_options = ng_json['projects']['angular-app']['architect']['build']['options'];
+  ng_json['projects']['angular-app']['architect']['build']['builder']=ng_json['projects']['angular-app']['architect']['build-ng']['builder'];
+  delete build_options['customWebpackConfig'];
+  build_options['styles'] = removeLazyEntries(build_options['styles']);
+  build_options['scripts'] = removeLazyEntries(build_options['scripts']);
+  build_options['lazyModules'] = [];
+  ng_json['projects']['angular-app']['architect']['build']['options'] = build_options;
+  fs.writeFileSync(src_path,JSON.stringify(ng_json, null, 4),'utf-8');
+}
+
 module.exports = {
   replaceAngularJson
 };
