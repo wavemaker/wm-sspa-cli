d9fa0385c9102ce6b9de3691780b36c18aac3d1f
Subodh Kumar ES <subodh.kumar@wavemaker.com>
2020-05-16 13:55:51 +0530

    Added a build conf

---
+++ index_cli.js
@@ -0,0 +1,61 @@
+const inquirer = require("./inquirer");
+const parseArgs = require("minimist");
+const {
+  initStatus,
+  endStatus,
+  printCliHeader,
+  printFailure
+} = require("./wm_cli_util");
+const { generateSspaBundle } = require("./wm_index");
+
+/*
+ * CLUI Settings
+ */
+
+printCliHeader();
+const trimEnd = path => (path.slice(-1) === "/" ? path.slice(0, -1) : path);
+let argv = parseArgs(process.argv.slice(2));
+if(argv["keep"] || argv["k"]){
+  process.env.KEEP_SSPA_PROJ = true;
+}
+if (argv["help"] || argv["h"]) {
+  printCliHeader();
+} else {
+  (async () => {
+    if (!argv["project-path"] && !argv["p"]) {
+      argv = { ...argv, ...(await inquirer.getProjectPath()) };
+    } else if (!inquirer.isValidPath(argv["project-path"] || argv["p"])) {
+      printFailure(
+        `Invalid WaveMaker project path: "${argv["project-path"] ||
+          argv["p"]}"\n`
+      );
+      return;
+    }
+    if (!argv["deploy-url"] && !argv["d"]) {
+      argv = { ...argv, ...(await inquirer.getDeployedUrl()) };
+    } else if (!(await inquirer.isValidURL(argv["deploy-url"] || argv["d"]))) {
+      printFailure(
+        `Invalid WaveMaker Deploy url:"${argv["deploy-url"] || argv["d"]}"\n`
+      );
+      return;
+    }
+
+    process.env.VERBOSE = argv["verbose"];
+    process.env.PROJECT_PATH = trimEnd(argv["project-path"] || argv["p"]);
+    process.env.DEPLOY_URL = trimEnd(argv["deploy-url"] || argv["d"]);
+    printCliHeader();
+    initStatus();
+    try {
+      await generateSspaBundle(
+        process.env.PROJECT_PATH,
+        process.env.DEPLOY_URL,
+        process.env.VERBOSE
+      );
+    } catch (e) {
+      printFailure(e);
+    } finally {
+      endStatus();
+      process.exit(1);
+    }
+  })();
+}
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require(`path`);
+const nodeExternals = require(`webpack-node-externals`);
+module.exports = {
+    entry: `./index_cli.js`,
+    output: {
+        path: path.resolve(__dirname,`dist`),
+        filename: `wm_bundle.js`
+    },
+    target: `node`,
+    externals: [nodeExternals()],
+    mode: `production`
+}
